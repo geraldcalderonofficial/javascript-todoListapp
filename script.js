fc906f3e9c9f9fa89bf8cd66e11b062075719d91
Gerald O. Calderon <95004398+geraldcalderonofficial@users.noreply.github.com>
2023-10-22 20:12:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ function get_todos() {
 }
  
 function add() {
-    
+    var task = document.getElementById('task').value;
+ 
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+ 
+    show();
+ 
+    return false;
 }
  
 function clearDefault(a) {
-if (a.defaultValue==a.value) {a.value=""}
+
 	
 };
 function remove() {
@@ -26,5 +34,17 @@ function remove() {
     return false;
 }
  
+function show() {
+    var todos = get_todos();
+ 
 
+    html += '</ul>';
+ 
+    document.getElementById('todos').innerHTML = html;
+ 
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
  
